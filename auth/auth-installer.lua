f713db7b20beb47757ddc309ccbcbb34065274b0
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2026-02-24 19:13:57 +0100

    Refactor Basalt installation to use download function
    Refactored installation process to use a download function for better code reuse and clarity.

--- auth/auth-installer.lua
+++ auth/auth-installer.lua
@@ -1,10 +1,16 @@
-print("Installing Basalt...")
-local request = http.get("https://raw.githubusercontent.com/Pyroxenium/Basalt2/refs/heads/main/release/basalt-full.lua")
-if not request then
-  error("Failed to download Basalt Full")
+local function download(file_name, url)
+    print("Downloading " .. file_name .. "...")
+    local request = http.get(url)
+    if not request then
+      error("Failed to download " .. file_name)
+    end
+    local file = fs.open(file_name, "w")
+    file.write(request.readAll())
+    file.close()
+    request.close()
+    print("Downloaded " .. file_name)
 end
-local file = fs.open("basalt.lua", "w")
-file.write(request.readAll())
-file.close()
-request.close()
-print("Basalt installed successfully!")
+
+download("basalt.lua", "https://raw.githubusercontent.com/Pyroxenium/Basalt2/refs/heads/main/release/basalt-full.lua")
+download("auth.lua", "https://raw.githubusercontent.com/mrLeonTheMaster/computercraft/refs/heads/main/auth/auth.lua")
+download("startup.lua", "https://raw.githubusercontent.com/mrLeonTheMaster/computercraft/refs/heads/main/auth/startup.lua")
