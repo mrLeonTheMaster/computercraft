c4814cdf84cb56d7bae19317ac0b159fcca97a70
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2026-02-25 21:55:07 +0100

    Refactor auth module and enhance startup process
    Updated authentication module and added terminal dimensions.

--- auth/startup.lua
+++ auth/startup.lua
@@ -1,4 +1,10 @@
 local main = require("main")
-local auth = require("auth")
-pcall(auth.auth, main.main)
+local auth = require("lib/auth")
+local auth_server = 16
+
+term_w, term_h = term.getSize()
+term.clear()
+term.setCursorPos((term_w/2)-5, term_h/2)
+term.write("Uruchamianie...")
+pcall(auth.auth, main.main, term_w, term_h, auth_server)
 os.reboot()
